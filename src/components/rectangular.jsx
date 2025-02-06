47730bc406e912f36736a4ab1a6d1b28e6cf89ab
Marckos <marcoshenrique.oliveira2023@gmail.com>
2025-02-06 12:24:12 -0300

    feat: creation of the rectangular component

---
+++ src/components/rectangular.jsx
@@ -0,0 +1,6 @@
+function Rectangular() {
+  return (
+    <div className="w-[55vh] h-[30vh] bg-custom-blue rounded-xl shadow-md"></div>
+  );
+}
+export default Rectangular;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#00EC82",
+        "custom-blue": "#42F8DA",
+      },
+    },
   },
   plugins: [],
 };
